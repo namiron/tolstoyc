eeee5b6b92ffc52289950183eb8fd1dffdb7aed0
namiron <sasha_zpua@mail.ru>
2024-08-20 17:50:34 +0300

    build

--- src/components/common/constant.tsx
+++ src/components/common/constant.tsx
@@ -1,18 +1,23 @@
 type ConstantType = string | number;
 
-export const baseUrl = `https://tolstoys.vercel.app`;
+export const baseUrl: ConstantType = `https://tolstoys.vercel.app`;
 
-export const DEFAULT_LOGIN = `login - userDefault@gmail.com`;
-export const DEFAULT_PASSWORD = ` password - user12345`;
+export const SEE_SITE: ConstantType = `See Site`;
 
-export const INPUT_FIRST = `First Url`;
+export const TOLSTOY: ConstantType = `Tolstoy`;
 
-export const INPUT_SECOND = `Second Url`;
+export const NO_DATA: ConstantType = `No data available`;
+
+export const DEFAULT_LOGIN: ConstantType = `login - userDefault@gmail.com`;
+
+export const DEFAULT_PASSWORD: ConstantType = ` password - user12345`;
+
+export const INPUT_FIRST: ConstantType = `First Url`;
+
+export const INPUT_SECOND: ConstantType = `Second Url`;
 
 export const URL_ERRORS: ConstantType = `You mast field this field`;
 
 export const SUBMIT: ConstantType = `Submit`;
 
 export const INPUT_THIRD = ` Third Url`;
-
-export const TOLSTOY = `Tolstoy`;
